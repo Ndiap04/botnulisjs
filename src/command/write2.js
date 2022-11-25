7c73ddd0dc1e712f8e709fd4ce36c39f387de3ff
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2022-11-25 19:54:04 +0700

    Create write2.js

---
+++ src/command/write2.js
@@ -0,0 +1,40 @@
+const { bot } = require("../bot");
+const TiaraShaktiGenerator = require("../generator/TiaraShakti");
+const { teleImgCompress } = require("../lib/Util");
+
+bot.command('write', (ctx) => {
+  ctx.replyWithHTML(ctx.locale['writejs_prompt'], { disable_web_page_preview: true });
+  ctx.userCache.awaitResponse = 'write';
+  ctx.toMenu(ctx, true);
+});
+
+bot.on('text', async (ctx, next) => {
+  const userCache = ctx.userCache;
+  if (!userCache.awaitResponse.startsWith('write')) return next();
+
+  ctx.reply(ctx.locale['writejs_wait']);
+  let Generator;
+  const paper = userCache.paper;
+  if (paper == 'images') {
+    Generator = new TiaraShaktiGenerator(userCache);
+  } else {
+    Generator = new TiaraShaktiGenerator(userCache);
+  }
+  Generator.image = paper;
+  try {
+    await Generator.loadImage();
+    await Generator.write(ctx.message.text);
+    // Generator.write(ctx.message.text, 130, 150);
+    for (const buff of Generator.buffers) {
+      const compressed = await teleImgCompress(buff);
+      await ctx.replyWithPhoto({ source: compressed });
+    }
+  } catch (e) {
+    logger.error(e.stack);
+  }
+
+  ctx.toMenu(ctx);
+  userCache.awaitResponse = '';
+
+  return next();
+});
